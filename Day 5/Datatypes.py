c6e7d68b782b143acbcbdcb82a3af39674249f27
gauravshelke <gauravshelke093@gmail.com>
2022-09-02 18:45:44 +0530

    datatypes of python

---
+++ Day 5/Datatypes.py
@@ -0,0 +1,145 @@
+name = "ABC"
+print(name)
+x = range(10)
+print(x)
+print(type(x))
+
+dict = {"name" : "Priti", "age" : 17}
+print(dict)
+print(type(dict))
+
+fruitSet= ({"apple", "pear", "grapes"})
+print(fruitSet)
+print(type(fruitSet))
+
+fruitFrozenSet= ({"apple", "pear", "grapes"})
+print(fruitFrozenSet)
+print(type(fruitFrozenSet))
+
+boolValue = True
+print(boolValue)
+print(type(boolValue))
+
+Bytes = b"this is bytes"
+
+byteArray = bytearray(10)
+print(byteArray)
+print(type(byteArray))
+
+NoneType=None
+print(NoneType)
+print(type(NoneType))
+
+tuple = ("apple", "pear", "grapes")
+print(tuple)
+print(type(tuple))
+
+fruitList=["apple", "pear", "grapes"]
+print(fruitList)
+print(type(fruitList))
+
+complexNumber = 10j
+print(complexNumber)
+print(type(complexNumber))
+
+floatNumber=10.5
+print(floatNumber)
+print(type(floatNumber))
+
+integerNumber=10
+print(integerNumber)
+print(type(integerNumber))
+
+# setting the specific data types
+
+name=str("ABC")
+print(name)
+print(type(name))
+
+int(100)
+print(int)
+print(type(int))
+
+float(20.5)
+print(float)
+print(type(float))
+
+complex=complex(100j)
+print(complex)
+print(type(complex))
+
+fruitList = list(("apple", "pear", "grapes"))
+print(fruitList)
+print(type(fruitList))
+
+''' fruitTuple = tuple(("apple", "pear", "grapes")) 
+print(fruitTuple)
+print(type(fruitTuple)) '''
+
+ranges=range(10)
+print(ranges)
+print(type(ranges))
+
+''' dictionary = dict(name="Priti", age=17)
+print(dictionary)
+print(type(dictionary)) '''
+
+fruitSet = set(("apple", "pear", "grapes"))
+print(fruitList)
+print(type(fruitList))
+
+fruitFrozenSet = frozenset(("apple", "banana", "cherry"))
+print(fruitFrozenSet)
+print(type(fruitFrozenSet))
+
+boolType= bool(False)
+print(boolType)
+print(type(boolType))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
