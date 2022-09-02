2a4c66d5afaed24f7c6b85ac85d9e88db21c96e8
gauravshelke <gauravshelke093@gmail.com>
2022-09-02 18:38:26 +0530

    list

---
+++ Day 7/lists.py
@@ -0,0 +1,75 @@
+'''#Creating a list
+a=[1,2,3,4,5 ]
+print(a)
+#We can create a list of different data types 
+b=[1,"pranav",12.5,True]
+print("list after updation is \t",b)
+#List slicing 
+friends=["pranav","yash","prasad","Vishva"]
+print(friends[0:3])
+#Negative slicing 
+print(friends[-4:])'''
+#list=[1,10,5,2,4]
+#List Functions 
+#list.sort()  sorts the original list 
+#list.reverse() reverses the original list
+#list.append(7) adds elemnt at the end of list
+#list1.insert(2,8) inserts elemnt at specified index 
+#list.pop() Removes element at end 
+#list.remove(10)Removes the specified element in list
+#print(len(list))#Calculates length of string.
+#list constructor()
+#list using conatructor .
+'''fruitlist = list(("mango","apple","banana","grapes"))
+print(fruitlist)'''
+'''fruitwithoutconstructor=["watermelon","banana"]
+print(type(fruitwithoutconstructor))
+fruitwithoutconstructor[0]="Mango"
+print(fruitwithoutconstructor)'''
+#newfruitlist=["apple","grapes","banana","oranges","cheery"]
+#newfruitlist[0:1]="grapes","orange"
+#print( newfruitlist)
+#Using insert() method
+#newfruitlist.insert(2,"hi ")
+#print( newfruitlist)
+#using append method to add element at the end of the list.
+#newfruitlist.append("how are you")
+#print(newfruitlist)
+#Using extend method to add new liat to existing one
+#newfruitlist.extend(list)
+#print(newfruitlist)
+#tuple=("my ","name ","is","pranav")
+#newfruitlist.extend(tuple)
+#print(newfruitlist)
+#remove method ()
+#newfruitlist.remove("apple")
+#print(newfruitlist)
+#list1=["apple","grapes","mango"]
+#list1.pop()
+#print(list1)
+#using del keyword.
+#deleting first iyem of list.
+#del list[0]
+#print(list)
+#deleting complete list
+#del tuple
+#print(tuple)
+#empty the list using clear 
+'''print(newfruitlist)
+newfruitlist.clear()
+print(newfruitlist)
+print(len(newfruitlist))
+newfruitlist.insert(0,2)
+print(newfruitlist[0])
+list2=[10,2,6,9,4,5,]
+list1.sort()
+list1.sort(reverse=True)
+print(list1)'''
+newlist=[0,1,2,3]
+newlist1=[4,5,6,7]
+a=5
+b=6
+a=b
+print(a)
+#newlist.extend(newlist1)
+print(newlist)
